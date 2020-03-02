de0087bc33959981780e79c07b3b6f5467b244cd
Austin Bliss <abliss2@live.com>
2020-03-02 15:48:23 -0500

    part-two-answer

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 function sum() {
 
-    
+
 
     var numOne = document.getElementById("numOne").value;
 
-    var numTwo = document.getElementById("numTwo").value;  
-    
-    var result = Number(numOne) + Number(numTwo)
-    
-    document.getElementById("result").innerHTML= result;
+    var numTwo = document.getElementById("numTwo").value;
+
+    var opSelector = document.getElementById("operator")
+
+    var operation = opSelector.options[opSelector.selectedIndex].value;
+
+
+    switch (operation) {
+        case "add":
+            var result = parseInt(numOne) + parseInt(numTwo)
+            break;
+        case "subtract":
+            var result = parseInt(numOne) - parseInt(numTwo)
+            break;
+        case "multiply":
+            var result = parseInt(numOne) * parseInt(numTwo)
+            break;
+        case "divide":
+            var result = parseInt(numOne) / parseInt(numTwo)
+            break;
+        default:
+            var result = "What are you doing silly?!"
+    }
+
+
+    document.getElementById("result").innerHTML = result;
 }
